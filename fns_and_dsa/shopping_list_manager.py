c31331b133a4b1c3aaaa93d75da94994449c7154
bethelehem Melaku <betim7995@gmail.com>
2026-03-08 11:53:43 -0700

    Add temperature conversion tool and shopping list manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -7,36 +7,24 @@ def display_menu():
 
 def main():
     shopping_list = []
-
     while True:
         display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
-            print(f"{item} added to the list.")
-
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"{item} removed from the list.")
-            else:
-                print("Item not found in the list.")
-
         elif choice == '3':
-            if not shopping_list:
-                print("Shopping list is empty.")
-            else:
-                print("Your shopping list:")
-                for item in shopping_list:
-                    print("-", item)
-
+            print("Current Shopping List:")
+            for item in shopping_list:
+                print(item)
         elif choice == '4':
             print("Goodbye!")
             break
-
         else:
             print("Invalid choice. Please try again.")
 
