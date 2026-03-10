6359d41ac61812f4e12e6c5d1effafa9ec99ae6b
bethelehem Melaku <betim7995@gmail.com>
2026-03-10 04:43:59 -0700

    Add BankAccount class implementation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,15 @@
-# bank_account.py
-
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  # private attribute
+        self.account_balance = initial_balance
 
-    # Deposit money
     def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
+        self.account_balance += amount
 
-    # Withdraw money
     def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
+        if amount <= self.account_balance:
+            self.account_balance -= amount
             return True
-        else:
-            return False
+        return False
 
-    # Display balance
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
